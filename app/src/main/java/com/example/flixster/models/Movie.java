86a4d59bddaf3eb8bef2ac7c7019ec6ff5db8a95
TibaAlanssari <(tibaalanssari53@gmail.com)>
2021-07-17 12:44:24 -0500

    added rating and youtube player

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,21 +3,33 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+
+    int movieID;
+    double rating;
     String posterPath;
     String backdropPath;
     String title;
     String overview;
+
+    //an empty constructor is needed for thr Parceler library
+    public Movie(){
+
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         backdropPath = jsonObject.getString("backdrop_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieID = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -42,5 +54,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieID() {
+        return movieID;
     }
 }
